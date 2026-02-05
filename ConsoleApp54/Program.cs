37fb2f5a5c537a7204c988b895b9fd25889b75f9
zommber <elnabajas5@outlook.com>
2026-02-05 00:20:23 -0600

    program

--- ConsoleApp54/Program.cs
+++ ConsoleApp54/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+
+Console.Write("Introduce el primer número: ");
+if (!double.TryParse(Console.ReadLine(), out var a))
+{
+    Console.WriteLine("Entrada inválida.");
+    return;
+}
+
+Console.Write("Introduce el segundo número: ");
+if (!double.TryParse(Console.ReadLine(), out var b))
+{
+    Console.WriteLine("Entrada inválida.");
+    return;
+}
+
+var suma = new Suma();
+var resta = new Resta();
+var multiplicacion = new Multiplicacion();
+
+Console.WriteLine($"Suma: {suma.Calcular(a, b)}");
+Console.WriteLine($"Resta: {resta.Calcular(a, b)}");
+Console.WriteLine($"Multiplicación: {multiplicacion.Calcular(a, b)}");
